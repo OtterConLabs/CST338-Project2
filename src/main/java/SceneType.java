09338f10e5f1b24e4ade9178555037c2268e428f
Yoko Mohr <ymohr@csumb.edu>
2026-07-20 21:44:02 -0700

    Add initial JavaFX framework, user model, and SQLite configuration

---
+++ src/main/java/SceneType.java
@@ -0,0 +1,18 @@
+/**
+ * [CST338 Project2]
+ * Identifies each distinct scene in the application.
+ * Add a new value here whenever you add a new screen.
+ *
+ * @author Yoko Mohr
+ * @since 7/20/2026
+ */
+public enum SceneType {
+//    MAIN,
+    LOGIN,
+    REGISTER,
+    DASHBOARD,
+    COURSE_LIST,
+    COURSE_EDIT,
+    ASSIGNMENT_LIST,
+    ASSIGNMENT_EDIT
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,36 @@
+/**
+ * [CST338 Brief one-sentence description of what this class does.]
+ *
+ * @author Yoko Mohr
+ * * @since 7/20/2026
+ */
+
+/**
+ * [CST338 ]
+ *
+ * @author Yoko Mohr
+ * @since 7/20/2026
+ */
+public class User {
+    private int userId;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private UserRole role;
+
+    User(int userId, String username, String firstName, String lastName,
+         String email, String password, UserRole role) {
+        this.userId = userId;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    //methods will be coming soon....
+
+}
