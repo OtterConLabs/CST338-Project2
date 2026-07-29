1be858a256536f83c9dff7800234422f128aaa48
jbrowning-otter <jbrowning@csumb.edu>
2026-07-28 17:59:37 -0700

    updated readme with my first branch, generated Assignment Dao and completed the first method

---
+++ src/main/java/AssignmentDao.java
@@ -0,0 +1,102 @@
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * SQL Queries
+ * @author Jordan Browning
+ * @since 7/24/2026
+ */
+public class AssignmentDao
+{
+    private final Connection conn;
+
+    /**
+     * store the connection
+     */
+    public AssignmentDao(Connection conn)
+    {
+        if (conn == null)
+        {
+            throw new IllegalArgumentException("null error.");
+        }
+
+        this.conn = conn;
+    }
+
+    /**
+     * Screens the Assignment, Builds the SQL statement and creates the SQL Command,
+     * executes SQL.
+     * Fill in placeholders, Verify SQL, ASK for ID.
+     *
+     * @param assignment The Assignment to insert.
+     * @return The generated assignment ID.
+     * @throws SQLException If a database error occurs.
+     */
+    public int insert(Assignment assignment) throws SQLException
+    {
+        //Screens the Assignment
+        reqAssignment(assignment);
+
+        //Builds the SQL statement
+        String sql = """
+
+            INSERT INTO assignments (
+        course_id,
+        title,
+        description,
+        due_date,
+        points_possible
+                    )
+                    VALUES (?, ?, ?
+            , ?, ?)
+    """;
+
+        //Creates the SQL Command and executes SQL
+        try (PreparedStatement statement = conn.prepareStatement(
+                sql,
+                Statement.RETURN_GENERATED_KEYS
+        ))
+        {
+
+            //Fill in the placeholders
+            setStatementValues(statement, assignment);
+
+            //Verify SQL
+            int modRows = statement.executeUpdate();
+
+            if (modRows != 1)
+            {
+                throw new SQLException(
+                        "Assignment insertion mod "
+                                + modRows
+                                + " rows."
+                );
+            }
+
+            //After inserting the row, ask for the new primary key
+            try (ResultSet generatedKeys = statement.getGeneratedKeys())
+            {
+
+                //Verify that an ID was returned
+                if (!generatedKeys.next())
+                {
+                    throw new SQLException(
+                            "insertion did not return ID."
+                    );
+                }
+
+                //Get the generated ID
+                int generatedId = generatedKeys.getInt(1);
+
+                //Assign the generated ID to the Assignment object
+                assignment.setAssignmentId(generatedId);
+
+                //Return the generated ID
+                return generatedId;
+            }
+        }
+    }
